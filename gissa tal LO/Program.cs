3d1b9c21ec2d09e98d8aca92c691b2ef40b15a54
Pingvu <89245202+Pingvu@users.noreply.github.com>
2021-10-15 13:42:24 +0200

    up

---
+++ gissa tal LO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gissa_tal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
