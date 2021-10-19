cae99514f6934bc8bc5fcb7c5d05c7b431531740
Johanness15 <jost04004@utb.vaxjo.se>
2021-10-19 13:51:16 +0200

    Update Program.cs

--- gissa tal LO/Program.cs
+++ gissa tal LO/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace gissa_tal
 {
@@ -6,7 +6,23 @@ namespace gissa_tal
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        Random slump= new Random();
+        int randy = slump.Next(100);
+        int nt= 0;
+        Console.WriteLine("Gissa ett tal");
+        int tal= int.Parse(Console.ReadLine());
+       while (tal != randy){
+           if (tal < randy){
+               Console.WriteLine("talet är för lågt");
+           }
+           if (tal > randy){
+               Console.WriteLine("talet är för högt");
+           }
+           nt +=1;
+       }
+            Console.WriteLine("det är rätt tal");
+            Console.WriteLine("här är antalet försök" + nt);
+            
         }
     }
 }
